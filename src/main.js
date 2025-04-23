260433f8102bbcc136e27babca912f74e688549f
roronoacheick <roronoacheick3@icloud.com>
2025-04-23 16:10:19 +0200

    Arena_movement (#1)
    * Add: Two plateforms
    
    * Add:  Movements
    
    * Set: The gravity and the plaform
    
    * Update of the platform
    
    * set the gravety and the jump

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import kaboom from "kaboom";
 
 const k = kaboom();
 
+
+
 k.loadSprite("jojo", "sprites/bean.png");
 
 scene("game", () => {
   k.onClick(() => k.addKaboom(k.mousePos()));
+
+  
+  
+  
+  
 
   // add platform
   add([
@@ -13,69 +20,70 @@ scene("game", () => {
     pos(0, height() - 48),
     outline(4),
     area(),
+    outline(4),
+    area(),
     body({ isStatic: true }),
     color(127, 200, 255),
+  ]); 
+
+  add([
+    rect(300, 35),
+    pos(50, 300), 
+    area(),
+    body({ isStatic: true }),
+    color(255, 105, 180), 
   ]);
+
+  add([
+    rect(300, 35),
+    pos(900, 300), 
+    area(),
+    body({ isStatic: true }),
+    color(255, 105, 180), 
+  ]);
+
   // putting together our player character
   const bean = add([sprite("jojo"), pos(80, 40), area(), body()]);
 
   // .jump() when "space" key is pressed
   onKeyPress("space", () => {
     if (bean.isGrounded()) {
-      bean.jump();
+      bean.jump(650);
     }
   });
-  setGravity(1600);
+  //Movement 
+  k.onKeyDown("left", () => {
+  bean.move(-160, 0);
+ });
+
+
+ k.onKeyDown("right",() => {
+  bean.move(160,0);
+ });
+
+  setGravity(1200);
   console.log(bean);
 
-  loop(1, () => {});
-
-  function spawnTree() {
-    // add tree
-    add([
-      rect(48, rand(24, 64)),
-      area(),
-      outline(4),
-      pos(width(), height() - 48),
-      anchor("botleft"),
-      color(255, 180, 255),
-      move(LEFT, 240),
-      "tree", // add a tag here
-    ]);
-
-    wait(rand(0.5, 1.5), () => {
-      spawnTree();
-    });
-  }
-
+  
   let score = 0;
-const scoreLabel = add([
-    text(score),
-    pos(24, 24)
-])
+  const scoreLabel = add([text(score), pos(24, 24)]);
 
-onUpdate(() => {
+  onUpdate(() => {
     score++;
     scoreLabel.text = score;
-});
+  });
 
   scene("lose", () => {
-    add([
-        text("Game Over"),
-        pos(center()),
-        anchor("center"),
-    ])
-})
+    add([text("Game Over"), pos(center()), anchor("center")]);
+  });
 
   bean.onCollide("tree", () => {
     addKaboom(bean.pos);
     shake();
-	go("lose");
+    go("lose");
   });
 
-  spawnTree();
-});
-
+  
+ });
 
 go("game");
-
